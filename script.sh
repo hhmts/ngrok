19b1fbca6b195f73d86c698bfd4c4a1c4d80b69c
Mai-Trinh Huynh-Ha <hhmtrinh@apcs.vn>
2019-12-24 14:50:43 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -34,13 +34,13 @@ unzip ngrok-stable-linux-amd64.zip
 #  ./ngrok authtoken 1S2XcTJUPYRYF1OvAkgXnJXfgo4_87JtRBcHB3fLkHy2BjRxr
 
 # server1
-./ngrok authtoken 1S43QgmnkodAGYfMpNoUTbMh7oT_4DzuPH9XA2cG8rmAHEeEM
+# ./ngrok authtoken 1S43QgmnkodAGYfMpNoUTbMh7oT_4DzuPH9XA2cG8rmAHEeEM
 
 #1st pp
 # ./ngrok authtoken 1V6rY7ye85KHvsudlR4uJzHEheS_6Fi1R5qUmC8KTx6BNx4R6
 
 #1st pp 3
-# ./ngrok authtoken 1VFPwrTzPP6XBsel8kVh7fRRGXq_2WidtcPaAcysovcXA2oS3
+./ngrok authtoken 1VFPwrTzPP6XBsel8kVh7fRRGXq_2WidtcPaAcysovcXA2oS3
 
 
 echo "ngrok stated"
