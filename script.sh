798efa1fd26ad8cfb8836bfe78c740b349945b01
Mai-Trinh Huynh-Ha <hhmtrinh@apcs.vn>
2020-01-14 13:41:54 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -43,10 +43,10 @@ unzip ngrok-stable-linux-amd64.zip
 # ./ngrok authtoken 1VVozRian0q5ltjoKW44kfufiQR_5QQ8r6wKVowCjmDPdPFne
 
 # 1st pp 3
-./ngrok authtoken 1VFPwrTzPP6XBsel8kVh7fRRGXq_2WidtcPaAcysovcXA2oS3
+# ./ngrok authtoken 1VFPwrTzPP6XBsel8kVh7fRRGXq_2WidtcPaAcysovcXA2oS3
 
 # mybelovedpaper1
-# ./ngrok authtoken 1WL7bkHQBaRHgmETy9M6bv1L06p_5ouuZHXJRbab12HdbfQ3E
+./ngrok authtoken 1WL7bkHQBaRHgmETy9M6bv1L06p_5ouuZHXJRbab12HdbfQ3E
 
 
 
